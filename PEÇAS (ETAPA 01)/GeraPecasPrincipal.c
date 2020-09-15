7b91c986a9e0de99fab268a2a3ac1bf9e841b8cf
Renan Alencar Dores <alenrenan@gmail.com>
2020-09-15 08:05:34 -0300

    Update GeraPecasPrincipal.c

--- PEÇAS (ETAPA 01)/GeraPecasPrincipal.c
+++ PEÇAS (ETAPA 01)/GeraPecasPrincipal.c
@@ -1,63 +1,112 @@
+
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <time.h>
+
+#define nPecas 28 //qtd de pecas
+
+void flush_in(); //limpa o buffer overflow
+
+//cria o tipo peca
+typedef struct Peca
+{
+
+    int lado1;
+    int lado2;
+
+} tipo_peca;
+
+typedef struct Domino
+{
+    tipo_peca domino[nPecas];
+
+} tipo_domino;
+
+//Chama as funções
+tipo_domino formaPecas(tipo_domino pecasFormadas);
+void mostraPecas(tipo_domino pecasFormadas);
+void imprimeEmbaralhado(tipo_domino pecasFormadas);
 
 int main()
 {
-    printf("==================================================\n\tBEM VINDO AO JOGO DE DOMINO - PUC/SP\n==================================================\n");
 
-    int escolhaMenu;
-    int nPecas = 7;
-    int todasPecas[] = {66, 65, 64, 63, 62, 61, 60, 55, 54, 53, 52, 51, 50, 44, 43, 42, 41, 40, 33, 32, 31, 30, 22, 21, 20, 11, 10, 00};
-
-label: //Ponto de referência do goto
+    int escolha;
+    tipo_domino pecasFormadas;
+    pecasFormadas = formaPecas(pecasFormadas);
 
     //Menu
-    printf("\n\n(1) Ver Pecas\n(2) Embaralhar e Distirbuir Pecas\n(3) Reorganizar pecas\n(4) Voltar para o Menu\n(5) Sair do jogo\n");
-    printf("\nESCOLHA: ");
-    scanf("%d", &escolhaMenu);
-
-    //Escolhas a partir do menu
-    if (escolhaMenu == 1) //Mostra Pecas
+    do
     {
-        printf("\nA pecas sao: \n");
-        for (int i = 0; i < 28; i++)
+        printf("\n(1) Mostrar Pecas\n");
+        printf("(2) Embaralhar\n");
+        printf("(3) Mostrar Pecas Embaralhadas\n");
+        printf("(4) Sair\n");
+        printf("Qual sua opcao? \n");
+        scanf("%d", &escolha);
+
+        switch (escolha)
         {
+        case 1:
+            mostraPecas(pecasFormadas);
+            break;
 
-            printf("[ %d ]", todasPecas[i]);
+        case 2:
+            printf("As pecas foram embaralhadas");
+            break;
+
+        case 3:
+            imprimeEmbaralhado(pecasFormadas);
+            break;
+
+        default:
+            break;
         }
-        goto label;
-    }
-    else if (escolhaMenu == 2) //Embraralha e Distribui as peças
-    {
-        //"Semente" que impede que os números sejam iguais
-        srand((unsigned int)time(0));
-        //srand(time(0));
+    } while (escolha != 4);
+}
 
-        printf("Suas Pecas:\n");
-        //Gera as peças de acordo com o número aleatório gerado
-        for (int i = 1; i <= nPecas; i++)
+tipo_domino formaPecas(tipo_domino pecasFormadas)
+{
+
+    int repet = 0;
+
+    for (int i = 0; i <= 6; i++)
+    {
+        for (int j = i; j <= 6; j++)
         {
-            printf("\nPECA %d: [ %d ]\t", i, todasPecas[rand() % 28]);
-            //Implementar o inicio do jogo (QUANDO O JOGADOR SELECIONA A PEÇA QUE QUER JOGAR)
+            if (repet < 28)
+            {
+                pecasFormadas.domino[repet].lado1 = i;
+                pecasFormadas.domino[repet].lado2 = j;
+            }
+            repet++;
         }
     }
-    else if (escolhaMenu == 3)
-    {
-        printf("\nReorganizando Pecas ...");
-        for (int i = 0; i < 28; i++)
-        {
+    return pecasFormadas;
+}
 
-            printf("[ %d ]", todasPecas[i]);
+//Função que imprime todas as peças organizadas
+void mostraPecas(tipo_domino pecasFormadas)
+{
+    for (int j = 0; j < 28; j++)
+    {
+        if (pecasFormadas.domino[j].lado2 == 6)
+        {
+            printf("[ %d:%d ]\n", pecasFormadas.domino[j].lado1, pecasFormadas.domino[j].lado2);
+        }
+        else
+        {
+            printf("[ %d:%d ]", pecasFormadas.domino[j].lado1, pecasFormadas.domino[j].lado2);
         }
     }
-    else if (escolhaMenu == 4) //Retorna ao menu (Mudar para o menu principal quando for implementado o menu incial completo)
+}
+//Função que gera as peças
+void imprimeEmbaralhado(tipo_domino pecasFormadas)
+{
+    srand((unsigned int)time(0));
+    
+    printf("Suas Pecas:\n");
+    for (int i = 0; i < nPecas; i++)
     {
-        goto label;
-    }
-    else if (escolhaMenu == 5)
-    {
-        printf("Saindo do Jogo . . .\n");
+        printf("[%d:%d]", pecasFormadas.domino[rand() % 28].lado1, pecasFormadas.domino[rand() % 28].lado2);
     }
 }
